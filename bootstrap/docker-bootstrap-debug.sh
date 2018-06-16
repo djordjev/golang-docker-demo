9df9cbd54df2a68b71420bf3c654e811cf1aa0c2
Djordje Vukovic <djordje@localhost.localdomain>
2018-06-16 19:26:34 +0200

    Added debug option

--- bootstrap/docker-bootstrap-debug.sh
+++ bootstrap/docker-bootstrap-debug.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
 # build
-go build -gcflags='-N -l' ./simple-http.go
-dlv exec ./simple-http --headless --listen=:2345 --api-version=2 --log
+#go build -gcflags='-N -l' ./simple-http.go
+#dlv exec ./simple-http --headless --listen=:2345 --api-version=2 --log
+dlv debug ./simple-http.go --headless --accept-multiclient --listen=:2345 --api-version=2 --log
